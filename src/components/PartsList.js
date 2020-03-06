23404768fea44a5c8c573e3edab9c8946aa49381
Sara R Mills <SaraRMills@gmail.com>
2020-03-06 15:54:22 -0500

    completed Parts component

--- src/components/PartsList.js
+++ src/components/PartsList.js
@@ -1,48 +1,18 @@
 import React, { Component } from "react";
 import slugify from 'slugify';
 import USCurrencyFormat from '../CurrencyFormat'
+import Parts from './Parts'
 
 class PartsList extends Component {
 
 
   render() {
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.props.selected[feature].name}
-              onChange={e => this.props.handleUpdateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
-
-      //PartsList AND Parts
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
-
+    
     return(
-    <form className="main__form">
-      <h2>Customize your laptop</h2>
-      {features}
-    </form>
+      <form className="main__form">
+        <h2>Customize your laptop</h2>
+        <Parts features={this.props.features} selected={this.props.selected} handleUpdateFeature={this.props.handleUpdateFeature} />
+      </form>
     )
   }
 }
