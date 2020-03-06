c9e966693a6fcb31e6dc9abbc103598d7fbd5cc3
Sara R Mills <SaraRMills@gmail.com>
2020-03-06 17:08:43 -0500

    added card component

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Header from './components/Header'
-import PartsList from './components/PartsList';
 import USCurrencyFormat from './CurrencyFormat'
+import Cart from './components/Cart'
+import PartsList from './components/PartsList'
 
 
 
@@ -46,27 +47,12 @@ class App extends Component {
   render() {
 
 
-    //Cart Part
-    const summary = Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = this.state.selected[feature];
-
-      //CartPart
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
-    //Total
-    const total = Object.keys(this.state.selected).reduce(
-      (acc, curr) => acc + this.state.selected[curr].cost,
-      0
-    );
+    
+   //Total
+  const total = Object.keys(this.state.selected).reduce(
+    (acc, curr) => acc + this.state.selected[curr].cost,
+    0
+  );
 
     //Headers AND entire Cart
     return (
@@ -76,7 +62,7 @@ class App extends Component {
           <PartsList features={this.props.features} selected={this.state.selected} handleUpdateFeature={this.updateFeature} />
           <section className="main__summary">
             <h2>Your cart</h2>
-            {summary}
+            <Cart selected={this.state.selected} />
             <div className="summary__total">
               <div className="summary__total__label">Total</div>
               <div className="summary__total__value">
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,1 +1,32 @@
-import React from 'react'
+import React, { Component} from 'react'
+import USCurrencyFormat from '../CurrencyFormat'
+
+class Cart extends Component{
+
+  render(){
+      
+    const summary = Object.keys(this.props.selected).map((feature, idx) => {
+        const featureHash = feature + '-' + idx;
+        const selectedOption = this.props.selected[feature];
+    
+        //CartPart has one return from map
+        return (
+        <div className="summary__option" key={featureHash}>
+            <div className="summary__option__label">{feature} </div>
+            <div className="summary__option__value">{selectedOption.name}</div>
+            <div className="summary__option__cost">
+            {USCurrencyFormat.format(selectedOption.cost)}
+            </div>
+        </div>
+        );
+    });
+  
+    return(
+      summary
+
+    )
+
+  }
+}
+
+export default Cart
