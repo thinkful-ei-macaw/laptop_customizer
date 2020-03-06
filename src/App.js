0d6c8d13f9d3887da02d1c2f751b6c5af15ed119
Sara R Mills <SaraRMills@gmail.com>
2020-03-06 14:57:06 -0500

    moved US currency to new component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React, { Component } from 'react';
 
 // Normalizes string as a slug - a string that is safe to use
 // in both URLs and html attributes
-import slugify from 'slugify';
+
 
 import './App.css';
 
 import Header from './components/Header'
-// import Parts from './components/Parts'
+import PartsList from './components/PartsList';
+import USCurrencyFormat from './CurrencyFormat'
 
 
-// This object will allow us to
-// easily convert numbers into US dollar values
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
-});
+
+
+
 
 class App extends Component {
   state = {
@@ -50,37 +48,7 @@ class App extends Component {
 
   //Parts List AND Selection
   render() {
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
 
-      //PartsList AND Parts
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
 
     //Cart Part
     const summary = Object.keys(this.state.selected).map((feature, idx) => {
@@ -109,10 +77,7 @@ class App extends Component {
       <div className="App">
        <Header />
         <main>
-          <form className="main__form">
-            <h2>Customize your laptop</h2>
-            {features}
-          </form>
+          <PartsList />
           <section className="main__summary">
             <h2>Your cart</h2>
             {summary}
